36db1ec4eb1950903d846d518501dabd940d3dec
rahul7276 <shanbhagrahul7276@gmail.com>
2026-02-17 21:11:57 +0530

    terra-code added

---
+++ aws_module_project/main.tf
@@ -0,0 +1,42 @@
+# dev 
+
+module "dev-app" {
+    source = "./my_app_infra_module"
+    my_env = "dev"
+    instance_type = "t2.micro"
+    ami = "ami-019715e0d74f695be" 
+    instance_count = 1
+}
+
+#prd
+module "prd-app" {
+    source = "./my_app_infra_module"
+    my_env = "prd"
+    instance_type = "t2.medium"
+    ami = "ami-019715e0d74f695be"
+    instance_count = 4
+}
+
+#stg
+module "stg-app" {
+    source = "./my_app_infra_module"
+    my_env = "stg"
+    instance_type = "t2.small"
+    ami = "ami-019715e0d74f695be"
+    instance_count = 1
+  
+}
+output "dev_app_public_ips" {
+  value = module.dev-app.ec2_instances_public_ips
+  description = "Public IPs of the dev environment EC2 instances"
+}
+
+output "prd_app_public_ips" {
+  value = module.prd-app.ec2_instances_public_ips
+  description = "Public IPs of the prd environment EC2 instances"
+}
+
+output "stg_app_public_ips" {
+  value = module.stg-app.ec2_instances_public_ips
+  description = "Public IPs of the stg environment EC2 instances"
+}
---
+++ aws_module_project/my_app_infra_module/variables.tf
@@ -0,0 +1,19 @@
+variable "my_env" {
+    description = "The environment for the app"
+    type = string
+}
+
+variable "instance_type" {
+    description = "value of the instance type"
+    type = string
+}
+
+variable "ami" {
+    description = "value of the ami"
+    type = string
+}
+
+variable "instance_count" {
+    description = "count of ec2"
+    type = number
+}
